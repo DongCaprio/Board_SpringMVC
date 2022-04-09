667e5c58d7b17e35d76cddd049e826eb5a54dd27
김동진 <dongjin403@naver.com>
2022-04-09 17:19:54 +0900

    주석 처리

--- src/kr/co/softcampus/mapper/BoardMapper.java
+++ src/kr/co/softcampus/mapper/BoardMapper.java
@@ -10,5 +10,7 @@ public interface BoardMapper {
 			"content_file, content_writer_idx, content_board_idx, content_date) " +
 			"values (content_seq.nextval, #{content_subject}, #{content_text}, #{content_file, jdbcType=VARCHAR}, " +
 			"#{content_writer_idx}, #{content_board_idx}, sysdate)")
+	//위에보면 content_type에 jdbcType=VARCHAR 가 있는데 db상에서 null을 허용해도 마이바티스에서 거부가 되어
+	//꼭 저것을 추가해주어야지 null값을 정상적으로 처리할 수 있다.
 	void addContentInfo(ContentBean writeContentBean);
 }
