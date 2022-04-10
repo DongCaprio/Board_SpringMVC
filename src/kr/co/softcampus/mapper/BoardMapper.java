0a156d7fdf8e74916e4322792e588fe881114112
김동진 <dongjin403@naver.com>
2022-04-10 15:37:34 +0900

    링크처리 @SelectKey를 통한 쿼리 먼저뽑기

--- src/kr/co/softcampus/mapper/BoardMapper.java
+++ src/kr/co/softcampus/mapper/BoardMapper.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 
 import kr.co.softcampus.beans.ContentBean;
 
 public interface BoardMapper {
 	
+	@SelectKey(statement = "select content_seq.nextval from dual", keyProperty = "content_idx", before = true, resultType = int.class)
+	
 	@Insert("insert into content_table(content_idx, content_subject, content_text, " +
 			"content_file, content_writer_idx, content_board_idx, content_date) " +
-			"values (content_seq.nextval, #{content_subject}, #{content_text}, #{content_file, jdbcType=VARCHAR}, " +
+			"values (#{content_idx}, #{content_subject}, #{content_text}, #{content_file, jdbcType=VARCHAR}, " +
 			"#{content_writer_idx}, #{content_board_idx}, sysdate)")
 	//위에보면 content_type에 jdbcType=VARCHAR 가 있는데 db상에서 null을 허용해도 마이바티스에서 거부가 되어
 	//꼭 저것을 추가해주어야지 null값을 정상적으로 처리할 수 있다.
